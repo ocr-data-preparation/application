38d05ee0583613b722a4eb4079ce44050f94734f
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2020-02-24 22:22:46 -0500

    add download dialog component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import UploadDialog from "./UploadDialog";
+import DownloadDialog from "./DownloadDialog";
 
 export default function Home() {
   return (
     <div>
       <UploadDialog />
+      <DownloadDialog />
     </div>
   );
 }

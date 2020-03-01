2c72885c43cf42b5fad303650755712efcd4eb06
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2020-03-01 22:30:09 +0700

    add download page without select directory option

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import UploadDialog from "./UploadDialog";
 import DownloadDialog from "./DownloadDialog";
+import DownloadDialogNoPath from "./DownloadDialogNoPath";
 
 export default function Home() {
   return (
     <div>
       <UploadDialog />
       <DownloadDialog />
+      <DownloadDialogNoPath />
     </div>
   );
 }

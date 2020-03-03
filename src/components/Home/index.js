0679ad2fa16424b3abeda4cfe8134458d37e2392
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2020-03-03 10:19:33 +0700

    refactor project structure

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,38 @@
 import React from "react";
-
-import UploadDialog from "./UploadDialog";
-import DownloadDialog from "./DownloadDialog";
-import DownloadDialogNoPath from "./DownloadDialogNoPath";
+import { Link } from "react-router-dom";
+import { Box, Grid } from "@material-ui/core";
+import Fab from "@material-ui/core/Fab";
+import Publish from "@material-ui/icons/Publish";
+import GetApp from "@material-ui/icons/GetApp";
+import history from "../History";
 
 export default function Home() {
   return (
     <div>
-      <UploadDialog />
-      <DownloadDialog />
-      <DownloadDialogNoPath />
+      <Grid container direction="column" justify="center" alignItems="center">
+        <h2>Choose your action</h2>
+        <Grid container direction="row" justify="center" alignItems="center">
+          <Box>
+            <Fab>
+              <Publish />
+            </Fab>
+            <div>Upload</div>
+          </Box>
+          <Box>
+            <Link to={"/warning"}>
+              <Fab onClick={() => history.push("/warning")}>
+                <GetApp />
+              </Fab>
+            </Link>
+            <div>Download</div>
+          </Box>
+        </Grid>
+        <Link to={"/help"}>
+          <Fab variant="extended" onClick={() => history.push("/help")}>
+            Need help? Click here
+          </Fab>
+        </Link>
+      </Grid>
     </div>
   );
 }

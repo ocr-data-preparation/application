0b558b79aca86b148028e195b4a1ccc353782d7c
elvinaang <13517079@std.stei.itb.ac.id>
2020-03-23 16:36:35 +0700

    button bisa berubah warna sesuai state

--- src/components/Split/Test.js
+++ src/components/Split/Test.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {Component} from "react";
 import Test2 from "./test2.json"
 
 function Test() {
     const testStr = JSON.stringify(Test2);
     var arr = JSON.parse(testStr);
+    var myStyle = {
+        backgroundColor:'yellow'
+    }
     
     function handleClick(nama) {
         console.log("hello " + nama);
@@ -11,12 +14,15 @@ function Test() {
 
     function changeState(arr, idX, idY) {
         arr.test[idX][idY] = !arr.test[idX][idY];
+        myStyle = {
+            backgroundColor:'red'
+        }
         console.log(arr.test[idX][idY]);
     }
 
     return(
         <div>
-            <button onClick={changeState.bind(this, arr, 0, 0)}>test</button>
+            <button onClick={changeState.bind(this, arr, 0, 0)} style={myStyle}>test</button>
         </div>
     );
 }

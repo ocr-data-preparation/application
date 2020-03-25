433254a1514c01eda212a2414ca01cc57466bdf5
elvinaang <13517079@std.stei.itb.ac.id>
2020-03-26 04:00:19 +0700

    new layout untuk option

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Warning from "./components/Warning";
 import Home from "./components/Home";
 import Help from "./components/Help";
 import Buttons from "./components/Split/Buttons";
-import Test from "./components/Split/Test";
-// import Split from "./components/Split";
+// import Test from "./components/Split/Test";
+import TestNewUI from "./components/Split/TestNewUI";
 import FillingInPaper from "./components/Help/FillingInPaper";
 import TakingPhoto from "./components/Help/TakingPhoto";
 import SubmittingTutorial from "./components/Help/SubmittingTutorial";
@@ -23,7 +23,7 @@ function App() {
       <Route exact path="/help" component={Help} />
       {/* <Route exact path="/split" component={Split} /> */}
       <Route exact path="/buttons" component={Buttons} />
-      <Route exact path="/test" component={Test} />
+      <Route exact path="/test" component={TestNewUI} />
       <Route path="/split/done" component={SplitDone} />
       <Route path="/help/paper" component={FillingInPaper} />
       <Route path="/help/take" component={TakingPhoto} />

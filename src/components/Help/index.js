06e6cd4ddf9844d9df6f0bdc73e94178e762de9b
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2020-04-06 20:53:34 +0700

    revamp filling paper component

--- src/components/Help/index.js
+++ src/components/Help/index.js
@@ -7,60 +7,54 @@ import FillingInPaper from "./FillingInPaper";
 import SubmittingTutorial from "./SubmittingTutorial";
 import TakingPhoto from "./TakingPhoto";
 
-const useStyles = makeStyles(theme => ({
-    container: {
-        marginTop: "3vw",
-        textAlign: "center",
-        direction: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "2vw"
-    },
-    root: {
-      backgroundColor:"#6E87FF"
-    },
-    title: {
-      textAlign: "center",
-      justifyContent: "center",
-      backgroundColor: 'white',
-      color: '#6E87FF',
-      fontSize: '2vw'
-    },
-    display: {
-      width: '70%',
-      padding: "3vw"
-    }
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: "3vw",
+    textAlign: "center",
+    direction: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "2vw",
+  },
+  title: {
+    textAlign: "center",
+    justifyContent: "center",
+    color: "white",
+    fontSize: "2vw",
+  },
+  display: {
+    width: "70%",
+    padding: "3vw",
+  },
 }));
 
 function Help() {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-          <Link to="/home">
-            <IconButton aria-label="close">
-              <ArrowBackRoundedIcon />
-            </IconButton>
-          </Link>
-          <Grid container className={classes.title}>
-            <h1>
-              TUTORIAL
-            </h1>
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Link to="/home">
+        <IconButton aria-label="close">
+          <ArrowBackRoundedIcon />
+        </IconButton>
+      </Link>
+      <Grid container className={classes.title}>
+        <h1>TUTORIAL</h1>
+      </Grid>
+      <Grid container className={classes.container}>
+        <Paper className={classes.display}>
+          <Grid>
+            <FillingInPaper />
           </Grid>
-          <Grid container className={classes.container}>
-            <Paper className={classes.display}>
-              <Grid>
-                <FillingInPaper/>
-              </Grid>
-              <Grid>
-                <TakingPhoto/>
-              </Grid>
-              <Grid>
-                <SubmittingTutorial/>
-              </Grid>
-            </Paper>
+          <Grid>
+            <TakingPhoto />
           </Grid>
-        </div>
-    );
+          <Grid>
+            <SubmittingTutorial />
+          </Grid>
+        </Paper>
+      </Grid>
+    </div>
+  );
 }
 
 export default Help;
